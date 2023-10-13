d599a7d9078206409b5f8c6c88d425a8140d1d01
aronweiler <aronweiler@gmail.com>
2023-10-13 00:29:31 -0700

    Conversation only or Auto mode

--- src/ai/rag_ai.py
+++ src/ai/rag_ai.py
@@ -4,6 +4,8 @@ from typing import List
 
 from langchain.base_language import BaseLanguageModel
 from langchain.agents import AgentExecutor
+from langchain.chains.llm import LLMChain
+from langchain.memory.readonly import ReadOnlySharedMemory
 
 from src.configuration.assistant_configuration import (
     RetrievalAugmentedGenerationConfiguration,
@@ -21,6 +23,7 @@ class RetrievalAugmentedGenerationAI:
 
     llm: BaseLanguageModel = None
     configuration: RetrievalAugmentedGenerationConfiguration
+    mode: str = "Auto"
 
     def __init__(
         self,
@@ -45,10 +48,22 @@ class RetrievalAugmentedGenerationAI:
             self.llm,
             self.configuration.model_configuration.max_conversation_history_tokens,
         )
-
-        self.tool_manager = ToolManager(
-            self.configuration, self.interaction_manager, self.llm
+        
+        memory = ReadOnlySharedMemory(
+            memory=self.interaction_manager.conversation_token_buffer_memory
         )
+        
+        self.chain = LLMChain(
+            llm=self.llm,
+            prompt=get_prompt(
+                self.configuration.model_configuration.llm_type, "CONVERSATIONAL_PROMPT"
+            ),
+            memory=memory,
+        )
+
+        # The tool manager contains all of the tools available to the AI
+        self.tool_manager = ToolManager()        
+        self.tool_manager.initialize_tools(self.configuration, self.interaction_manager, self.llm)
 
     def create_agent(self, agent_timeout: int = 300):
         tools = self.tool_manager.get_enabled_tools()
@@ -96,6 +111,7 @@ class RetrievalAugmentedGenerationAI:
         query: str,
         collection_id: int = None,
         agent_callbacks: List = [],
+        llm_callbacks: List = [],
         kwargs: dict = {},
     ):
         # Set the document collection id on the interaction manager
@@ -108,22 +124,13 @@ class RetrievalAugmentedGenerationAI:
         logging.debug("Checking to see if summary exists for this chat")
         self.check_summary(query=query)
 
-        timeout = kwargs.get("agent_timeout", 300)
-        logging.debug(f"Creating agent with {timeout} second timeout")
-        agent = self.create_agent(agent_timeout=timeout)
-
-        # Run the agent
-        logging.debug("Running agent")
-        results = agent.run(
-            input=query,
-            system_information=get_system_information(
-                self.interaction_manager.user_location
-            ),
-            user_name=self.interaction_manager.user_name,
-            user_email=self.interaction_manager.user_email,
-            callbacks=agent_callbacks,
-        )
-        logging.debug("Agent finished running")
+        if self.mode == "Conversation":
+            logging.debug("Running chain 'Conversation Only' mode")
+            results = self.run_chain(query=query, llm_callbacks=llm_callbacks, kwargs=kwargs)
+        else:
+            # Run the agent
+            logging.debug("Running agent 'Auto' mode")
+            results = self.run_agent(query=query, agent_callbacks=agent_callbacks, kwargs=kwargs)
 
         # Adding this after the run so that the agent can't see it in the history
         self.interaction_manager.conversation_token_buffer_memory.chat_memory.add_user_message(
@@ -139,6 +146,39 @@ class RetrievalAugmentedGenerationAI:
 
         return results
 
+    def run_chain(self, query: str, llm_callbacks: List = [], kwargs: dict = {}):
+        return self.chain.run(
+            system_prompt="You are a friendly AI who's purpose it is to engage a user in conversation.  Try to mirror their emotional state, and answer their questions.  If you don't know the answer, don't make anything up, just say you don't know.",
+            input=query,
+            user_name=self.interaction_manager.user_name,
+            user_email=self.interaction_manager.user_email,
+            system_information=get_system_information(
+                self.interaction_manager.user_location
+            ),
+            context="N/A",
+            loaded_documents="\n".join(
+                self.interaction_manager.get_loaded_documents_for_display()
+            ),
+            callbacks=llm_callbacks,
+        )
+
+    def run_agent(self, query: str, agent_callbacks: List = [], kwargs: dict = {}):
+        timeout = kwargs.get("agent_timeout", 300)
+        logging.debug(f"Creating agent with {timeout} second timeout")
+        agent = self.create_agent(agent_timeout=timeout)
+
+        # Run the agent
+        logging.debug("Running agent")
+        return agent.run(
+            input=query,
+            system_information=get_system_information(
+                self.interaction_manager.user_location
+            ),
+            user_name=self.interaction_manager.user_name,
+            user_email=self.interaction_manager.user_email,
+            callbacks=agent_callbacks,
+        )
+
     def check_summary(self, query):
         if self.interaction_manager.interaction_needs_summary:
             logging.debug("Interaction needs summary, generating one now")
@@ -151,3 +191,11 @@ class RetrievalAugmentedGenerationAI:
             self.interaction_manager.set_interaction_summary(interaction_summary)
             self.interaction_manager.interaction_needs_summary = False
             logging.debug(f"Generated summary: {interaction_summary}")
+            
+    def set_mode(self, mode: str):        
+        if mode == "Conversation Only":
+            # Use the LLM with a chat prompt
+            self.mode = "Conversation"
+        else:    
+            # Normal mode, let the AI decide what to do (agent)
+            self.mode = "Auto"
