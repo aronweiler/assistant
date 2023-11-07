5193c20cd8d850a918865cab42b99dddf6ea4c8c
aronweiler <aronweiler@gmail.com>
2023-11-06 21:21:32 -0800

    Update Dockerignore, install script, and requirements.txt

--- install.ps1
+++ install.ps1
@@ -1,10 +1,5 @@
 python -m venv assistant_venv
 Invoke-Expression -Command .\assistant_venv\Scripts\activate
 python.exe -m pip install --upgrade pip
-# Comment the following lines (through the torch install) if you don't have a GPU that you want to use with llamacpp
-$env:CMAKE_ARGS="-DLLAMA_CUBLAS=on"      
-$env:FORCE_CMAKE=1
-$env:LLAMA_CUBLAS=1 
-pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
-pip cache purge
-pip --no-cache-dir install -r requirements.txt
+
+pip install -r requirements.txt
