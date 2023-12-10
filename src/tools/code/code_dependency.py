39943049cd0deaa0cd44dc5b88357a26220aa717
Aron Weiler <aronweiler@gmail.com>
2023-12-10 00:58:44 -0800

    Refactor code and add documentation

--- src/tools/code/code_dependency.py
+++ src/tools/code/code_dependency.py
@@ -1,9 +1,20 @@
-from typing import List
+from typing import List, Optional
 
 class CodeDependency:
-    name: str
-    dependencies: List["CodeDependency"]
+    """Represents a code dependency.
 
-    def __init__(self, name: str, dependencies: List["CodeDependency"] = []):
+    Attributes:
+        name (str): The name of the dependency.
+        dependencies (List["CodeDependency"]): A list of dependencies for this code dependency.
+    """
+
+    def __init__(self, name: str, dependencies: Optional[List["CodeDependency"]] = None):
+        """Initializes a new instance of the CodeDependency class.
+
+        Args:
+            name (str): The name of the dependency.
+            dependencies (Optional[List["CodeDependency"]]): A list of dependencies for this code dependency.
+                If None, it will be initialized as an empty list.
+        """
         self.name = name
-        self.dependencies = dependencies
+        self.dependencies = dependencies if dependencies is not None else []
