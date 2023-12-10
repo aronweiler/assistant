cf2fed444fa47feff10e75b1c0d4ddfc20d34666
aronweiler <aronweiler@gmail.com>
2023-12-10 10:09:36 -0800

    Add GitlabCommitter class for committing changes to GitLab repository

---
+++ src/integrations/gitlab/gitlab_committer.py
@@ -0,0 +1,64 @@
+import logging
+from typing import List
+import gitlab
+import src.integrations.gitlab.gitlab_shared as gitlab_shared
+
+
+class GitlabCommitter:
+    def __init__(self, source_control_url: str, source_control_pat: str):
+        self.gitlab = gitlab_shared.retrieve_gitlab_client(
+            source_control_url, source_control_pat, verify_auth=True
+        )
+
+    def commit_changes(
+        self,
+        source_branch,
+        target_branch,
+        repository,
+        commit_message,
+        code_and_file_paths: List[dict],
+    ):
+        project = self.gitlab.projects.get(repository)
+
+        # Get the latest commit of the target_branch
+        try:
+            target_branch_commit = project.branches.get(target_branch).commit
+        except gitlab.exceptions.GitlabGetError:
+            # If target_branch does not exist, start from source_branch
+            source_branch_commit = project.branches.get(source_branch).commit
+            target_branch_commit = source_branch_commit
+
+        actions = [
+            {
+                "action": "update",
+                "file_path": code_and_file_path["file_path"],
+                "content": code_and_file_path["code"],
+            }
+            for code_and_file_path in code_and_file_paths
+        ]
+
+        # Create a new commit in the repository
+        commit = project.commits.create(
+            {
+                "branch": target_branch,
+                "commit_message": commit_message,
+                "actions": actions,
+                "start_branch": source_branch,
+            }
+        )
+
+        logging.info("Changes committed and pushed successfully!")
+
+    def _create_branch(self, project, source_branch, target_branch):
+        try:
+            branch = project.branches.get(target_branch)
+            logging.info(f"Branch '{target_branch}' already exists!")
+            return branch
+        except gitlab.exceptions.GitlabGetError:
+            pass
+
+        branch = project.branches.create(
+            {"branch": target_branch, "ref": source_branch}
+        )
+        logging.info(f"New branch '{target_branch}' created successfully!")
+        return branch
