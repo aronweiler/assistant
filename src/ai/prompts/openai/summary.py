0ea51fb326acc350107c1bb891da33ebfb478d2c
Aron Weiler <aronweiler@gmail.com>
2023-12-19 10:51:47 -0800

    Read webpage (#87)
    * Added website reading tool.  Update model and tool configurations
    
    * updated config
    
    * better summary prompts
    
    * updated website tool to split text
    
    * Update additional instructions for get_text_from_website function
    
    * Fix condition to handle empty questions in Documents class
    
    * Add Documentation Runner
    
    * Bumped version from 0.66 to 0.67
    
    * Add release notes for Jarvis 0.67
    
    ---------
    
    Co-authored-by: Aron Weiler <aron.j.weiler@medtronic.com>

--- src/ai/prompts/openai/summary.py
+++ src/ai/prompts/openai/summary.py
@@ -32,13 +32,20 @@ DETAILED SUMMARY:
 
 DETAILED_SUMMARIZE_PROMPT = PromptTemplate.from_template(DETAILED_SUMMARIZE_TEMPLATE)
 
-SIMPLE_REFINE_TEMPLATE = """Your job is to produce a final summary of the following text with the goal of answering a user's query. Below is provided an existing summary up to a certain point: 
+SIMPLE_REFINE_TEMPLATE = """Your job is to produce a final summary of the following text so that another AI can use your summary to answer the user's query. Below is provided an existing summary up to a certain point: 
 
 ----- BEGIN EXISTING SUMMARY -----
 {existing_answer}
 ----- END EXISTING SUMMARY -----
 
 Now you have the opportunity to refine or enhance the existing summary (only if needed) with some more context below.
+
+Please consider these guidelines when refining the summary:
+- If the additional context directly addresses the user's query, integrate this information succinctly into the existing summary.
+- If there are discrepancies between the existing summary and additional context, resolve them for accuracy.
+- Maintain neutrality and objectivity in the summary, avoiding any personal opinions or interpretations.
+- Ensure that the final summary is coherent and flows logically from start to finish.
+- Use bullet points or numbered lists where appropriate to enhance readability and organization.
 
 ----- BEGIN ADDITIONAL CONTEXT -----
 {text}
@@ -48,17 +55,48 @@ Now you have the opportunity to refine or enhance the existing summary (only if 
 {query}
 ----- END USER QUERY -----
 
-If the additional context contains information relevant to the user's query, use it to add additional information to the summary.  If the additional context isn't useful, or is unrelated to the user's query, just return the existing summary.
+After reviewing both the existing summary and additional context:
+- If the additional context contains information relevant to the user's query, use it to add essential details or clarify points in the existing summary.
+- If the additional context isn't useful, or is unrelated to the user's query, confirm that the existing summary sufficiently answers the query and return it without changes.
+- In cases where neither the existing summary nor additional context address the user's query, please indicate that no relevant information is available.
+
+By adhering to these guidelines, you will help ensure that the final summary is not only informative and relevant but also clear and professional in its presentation.
+
+Remember not to fabricate any information. If you do not know something or if certain information is missing, simply state that you don't know or that information is not provided.
+
+If the additional context is not useful, or is unrelated to the user's query, return the existing summary without changes.
+
+AI: 
 """
 
 SIMPLE_REFINE_PROMPT = PromptTemplate.from_template(SIMPLE_REFINE_TEMPLATE)
 
 
-DETAILED_DOCUMENT_CHUNK_SUMMARY_TEMPLATE = """Write a detailed summary of the following piece of a larger document:
+DETAILED_DOCUMENT_CHUNK_SUMMARY_TEMPLATE = """Your job is to write a detailed summary of the following piece of a larger document. This summary should capture all key points and relevant details to provide a solid foundation for understanding the document's content. The summary will be used by another AI or user to grasp the essence of this section without reading it in full.
 
+Please adhere to these guidelines when creating the summary:
+- Identify and highlight the main ideas, arguments, or events presented in the text.
+- Include any important data, statistics, names, dates, or locations that are crucial for understanding the context.
+- Maintain neutrality and objectivity, avoiding personal opinions or interpretations.
+- Ensure that your summary is coherent and flows logically from start to finish.
+- Use bullet points or numbered lists where appropriate to organize information clearly.
+
+----- BEGIN DOCUMENT CHUNK -----
 {text}
+----- END DOCUMENT CHUNK -----
 
 DETAILED SUMMARY:
+
+After reviewing the text:
+- Provide a concise yet comprehensive overview that encapsulates all significant aspects of the document chunk.
+- If certain information is implied but not explicitly stated in the text, you may include such inferences, clearly marking them as interpretations.
+- Avoid including extraneous details that do not contribute to a fundamental understanding of the text.
+
+Remember not to fabricate any information. If you do not know something or if certain information is missing from the chunk provided, simply state that specific details are not available within this section.
+
+By adhering to these guidelines, you will help ensure that the initial summary is informative, accurate, and useful for anyone who needs to understand this part of the document without reading it in full.
+
+AI:
 """
 
 SIMPLE_DOCUMENT_REFINE_TEMPLATE = """Your job is to produce a final summary of an entire document that has been split into chunks. You will be provided a summary of all prior chunks, and one additional chunk.
---
+++ src/tools/web/website_tool.py
@@ -0,0 +1,95 @@
+import requests
+from typing import List
+from bs4 import BeautifulSoup
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+
+# Add the project root to the python path at runtime
+import sys
+from pathlib import Path
+from src.ai.conversations.conversation_manager import ConversationManager
+from src.ai.llm_helper import get_tool_llm
+
+sys.path.append(str(Path(__file__).parent.parent.parent))
+
+from src.utilities.token_helper import num_tokens_from_string
+
+
+class WebsiteTool:
+    def __init__(
+        self,
+        configuration,
+        conversation_manager: ConversationManager,
+    ):
+        """
+        Initializes the WebsiteTool with a given configuration and an conversation manager.
+
+        :param configuration: Configuration settings for the tool.
+        :param conversation_manager: The manager that handles interactions with language models.
+        """
+        self.configuration = configuration
+        self.conversation_manager = conversation_manager
+
+    def get_text_from_website(self, url: str, user_query: str) -> str:
+        """Reads content from a website"""
+        full_html = requests.get(url).text
+
+        # parse using beautiful soup
+        soup = BeautifulSoup(full_html, "html.parser")
+        raw_text_no_html = soup.get_text(strip=True, separator=" ")
+
+        return self.get_summary_or_text(raw_text_no_html, url, user_query)
+
+    def get_summary_or_text(self, text: str, url: str, user_query: str) -> str:
+        """Returns a summary of the text if it is larger than the max chunk size, otherwise returns the text"""
+        additional_settings = self.configuration["tool_configurations"][
+            self.get_text_from_website.__name__
+        ]["additional_settings"]
+
+        max_chunk_size = additional_settings["max_chunk_size"]["value"]
+
+        num_tokens = num_tokens_from_string(text)
+        if num_tokens > max_chunk_size:
+            summary = self.get_summary(
+                text=text, user_query=user_query, max_chunk_size=max_chunk_size
+            )
+            return f"The chunk size of {max_chunk_size} was exceeded, so here is a summary of the user's request for {url}:\n\n{summary}"
+        else:
+            return f"Here is the raw text of {url}:\n\n{text}"
+
+    def get_summary(self, text: str, user_query: str, max_chunk_size: int) -> str:
+        """Returns a summary of the text"""
+
+        splitter = RecursiveCharacterTextSplitter(
+            chunk_size=max_chunk_size,
+            chunk_overlap=50,
+            length_function=num_tokens_from_string,
+        )
+
+        split_text = splitter.split_text(text)
+
+        # Initialize language model for prediction.
+        llm = get_tool_llm(
+            configuration=self.configuration,
+            func_name=self.get_text_from_website.__name__,
+            streaming=True,
+        )
+
+        existing_summary = None
+
+        for chunk in split_text:
+            if not existing_summary:
+                prompt = self.conversation_manager.prompt_manager.get_prompt(
+                    category="summary",
+                    prompt_name="DETAILED_DOCUMENT_CHUNK_SUMMARY_TEMPLATE",
+                ).format(existing_answer=existing_summary, text=chunk, query=user_query)
+            else:
+                prompt = self.conversation_manager.prompt_manager.get_prompt(
+                    category="summary", prompt_name="SIMPLE_REFINE_TEMPLATE"
+                ).format(existing_answer=existing_summary, text=chunk, query=user_query)
+
+            existing_summary = llm.predict(
+                prompt=prompt, callbacks=self.conversation_manager.agent_callbacks
+            )
+
+        return existing_summary
