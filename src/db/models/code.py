ad6ced87195608023ab0f118aec02dc738d534c4
aronweiler <aronweiler@gmail.com>
2023-12-24 22:56:12 -0800

    Add logging import and update code file model
    Add code file existence check and update code file method
    
    Add code file creation and update method
    
    Add code file update logic
    
    Add code file creation logic
    
    Add code file keyword and description creation logic

--- src/db/models/code.py
+++ src/db/models/code.py
@@ -1,3 +1,4 @@
+import logging
 import sys
 import os
 from datetime import datetime
@@ -14,7 +15,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.
 from src.db.models.domain.code_repository_model import CodeRepositoryModel
 from src.db.models.domain.code_file_model import CodeFileModel
 
-from src.db.database.tables import CodeRepository, CodeFile
+from src.db.database.tables import (
+    CodeDescription,
+    CodeKeyword,
+    CodeRepository,
+    CodeFile,
+)
 
 from src.db.models.vector_database import VectorDatabase, SearchType
 from src.db.models.domain.document_collection_model import DocumentCollectionModel
@@ -36,21 +42,23 @@ class Code(VectorDatabase):
             ).all()
 
             return [CodeRepositoryModel.from_database_model(c) for c in repositories]
-        
+
     def get_repository(self, code_repo_id: int) -> CodeRepositoryModel:
         if code_repo_id is None or int(code_repo_id) == -1:
             return None
-        
+
         with self.session_context(self.Session()) as session:
-            repository = session.query(
-                CodeRepository.id,
-                CodeRepository.code_repository_address,
-                CodeRepository.branch_name,
-                CodeRepository.last_scanned,
-                CodeRepository.record_created,
-            ).filter(
-                CodeRepository.id == code_repo_id
-            ).one()
+            repository = (
+                session.query(
+                    CodeRepository.id,
+                    CodeRepository.code_repository_address,
+                    CodeRepository.branch_name,
+                    CodeRepository.last_scanned,
+                    CodeRepository.record_created,
+                )
+                .filter(CodeRepository.id == code_repo_id)
+                .one()
+            )
 
             return CodeRepositoryModel.from_database_model(repository)
 
@@ -74,13 +82,100 @@ class Code(VectorDatabase):
             )
             session.commit()
 
-
-    def add_code(self, file_path:str, code:str, keywords_and_descriptions:dict):
+    def code_file_exists(self, code_repo_id: int, file_sha: str) -> bool:
         with self.session_context(self.Session()) as session:
-            session.add(
-                CodeFile(
-                    file_path=file_path,
-                    code=code,
-                    keywords_and_descriptions=keywords_and_descriptions,
-                )
+            return (
+                session.query(CodeFile)
+                .filter(CodeFile.code_repository_id == code_repo_id)
+                .filter(CodeFile.code_file_sha == file_sha)
+                .count()
+                > 0
             )
+
+    def add_update_code(
+        self,
+        repository_id,
+        file_name: str,
+        file_sha: str,
+        file_content: str,
+        file_summary: str,
+        keywords_and_descriptions: dict,
+    ):
+        # Create the code file, adding the keywords and the descriptions as well
+        with self.session_context(self.Session()) as session:
+            
+            # If the code file already exists, then update it
+            existing_code_file = (
+                session.query(CodeFile)
+                .filter(CodeFile.code_repository_id == repository_id)
+                .filter(CodeFile.code_file_name == file_name)
+                .one_or_none()
+            )
+            
+            if existing_code_file:                
+                if existing_code_file.code_file_sha == file_sha:
+                    # The file has not changed, so we don't need to update it
+                    logging.info(f"Code file {file_name} has not changed, so we don't need to update it")
+                    return
+                
+                logging.info(f"Code file {file_name} has changed, so we need to update it")
+                
+                existing_code_file.code_file_sha = file_sha
+                existing_code_file.code_file_content = file_content
+                existing_code_file.code_file_summary = file_summary
+                existing_code_file.code_file_summary_embedding = get_embedding(
+                    text=file_summary,
+                    collection_type="Remote",
+                    instruction="Represent the summary for retrieval: ",
+                )
+                
+                session.add(existing_code_file)
+                session.commit()
+                
+                # Delete the existing keywords and descriptions
+                session.query(CodeKeyword).filter(
+                    CodeKeyword.code_file_id == existing_code_file.id
+                ).delete()
+                session.query(CodeDescription).filter(
+                    CodeDescription.code_file_id == existing_code_file.id
+                ).delete()
+                session.commit()
+            else:
+                logging.info(f"Code file {file_name} does not exist, so we need to create it")
+                
+                # Create the code file
+                code_file = CodeFile(
+                    code_repository_id=repository_id,
+                    code_file_name=file_name,
+                    code_file_sha=file_sha,
+                    code_file_content=file_content,
+                    code_file_summary=file_summary,
+                    # TODO: Implement embedding model selection for code
+                    code_file_summary_embedding=get_embedding(
+                        text=file_summary,
+                        collection_type="Remote",
+                        instruction="Represent the summary for retrieval: ",
+                    ),
+                )
+                session.add(code_file)            
+                session.commit()
+
+            for keyword in keywords_and_descriptions["keywords"]:
+                code_keyword = CodeKeyword(code_file_id=code_file.id, keyword=keyword)
+
+                session.add(code_keyword)
+                session.commit()
+
+            for description in keywords_and_descriptions["descriptions"]:
+                code_description = CodeDescription(
+                    code_file_id=code_file.id,
+                    description_text=description,
+                    description_text_embedding=get_embedding(
+                        text=description,
+                        collection_type="Remote",
+                        instruction="Represent the description for retrieval: ",
+                    ),
+                )
+
+                session.add(code_description)
+                session.commit()
