10431896cb3aa277309aededd87dd801182a90e9
aronweiler <aronweiler@gmail.com>
2023-12-24 21:54:40 -0800

    Add prompt for extracting keywords and descriptions from code

--- src/ai/prompts/openai/code_general.py
+++ src/ai/prompts/openai/code_general.py
@@ -6,3 +6,31 @@ CODE_PROMPT = PromptTemplate(
     template=CODE_PROMPT_TEMPLATE,
     input_variables=["page_content", "filename", "file_id", "start_line"],
 )
+
+CODE_DETAILS_EXTRACTION_TEMPLATE = """Please examine the following code carefully:
+
+----- CODE -----
+{code}
+----- CODE -----
+
+I would like you to extract a list of keywords that can be used to later search this code. The keywords should include things like variable names, function names, class names, and other items of interest. Please also include short descriptions (no more than a sentence or two) of the goals and functionality that is represented in this code, being sure to capture any functionality that could be of interest to someone searching the codebase.
+
+Your output should be in a JSON blob with the following format:
+
+```json
+{{
+    "keywords": [
+        "list",
+        "of",
+        "keywords"
+    ],
+    "descriptions": [
+        "short description 1",
+        "short description 2",
+        "etc."        
+    ]
+}}
+```
+
+AI: Sure, here is a JSON blob with the keywords and descriptions:
+"""
