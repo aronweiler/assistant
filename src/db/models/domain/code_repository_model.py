694179e4f0dc2b1955873dda3261a4f5d10d5bb8
aronweiler <aronweiler@gmail.com>
2023-12-24 21:55:13 -0800

    Add last_scanned attribute to CodeRepositoryModel class

--- src/db/models/domain/code_repository_model.py
+++ src/db/models/domain/code_repository_model.py
@@ -2,17 +2,20 @@ from src.db.database.tables import CodeRepository
 
 
 class CodeRepositoryModel:
-    def __init__(self, id, code_repository_address, branch_name, record_created=None):
+    def __init__(self, id, code_repository_address, branch_name, last_scanned = None, record_created=None):
         self.id = id
         self.code_repository_address = code_repository_address
         self.branch_name = branch_name
+        self.last_scanned = last_scanned
         self.record_created = record_created
+        
 
     def to_database_model(self):
         return CodeRepository(
             id=self.id,
             code_repository_address=self.code_repository_address,
             branch_name=self.branch_name,
+            last_scanned = self.last_scanned,
             record_created=self.record_created,
         )
 
@@ -25,5 +28,6 @@ class CodeRepositoryModel:
             id=db_code_repository.id,
             code_repository_address=db_code_repository.code_repository_address,
             branch_name=db_code_repository.branch_name,
+            last_scanned = db_code_repository.last_scanned,
             record_created=db_code_repository.record_created,
         )
