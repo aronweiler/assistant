9dddb5273fbb1422491e7bd0a2abb46b197f5a48
aronweiler <aronweiler@gmail.com>
2024-01-11 10:26:13 -0800

    Add new tools and update weather tool

---
+++ src/tools/tool_results/tool_results.py
@@ -0,0 +1,35 @@
+from typing import List
+from src.ai.conversations.conversation_manager import ConversationManager
+from src.ai.tools.tool_registry import register_tool
+from src.db.models.conversations import Conversations
+
+
+@register_tool(
+    display_name="Get Previous Tool Call Results",
+    requires_documents=False,
+    description="Get a previous tool call result (or results) by ID.",
+    additional_instructions="Call this tool when you need to get the results of a previous tool call or calls.",
+)
+def get_previous_tool_call_results(tool_call_result_ids: List[int]):
+    """
+    Get a previous tool call result (or results) by ID.
+
+    :param tool_call_result_ids: The IDs of the tool call result to retrieve.
+    """
+
+    if not isinstance(tool_call_result_ids, list):
+        tool_call_result_ids = [tool_call_result_ids]
+
+    conversations_helper = Conversations()
+
+    results = []
+    for tool_call_result_id in tool_call_result_ids:
+        tool_call = conversations_helper.get_tool_call_results_by_id(
+            tool_call_result_id
+        )
+
+        results.append(
+            f"{tool_call.record_created} - (ID: {tool_call.id}) Name: `{tool_call.tool_name}`, tool input: {tool_call.tool_arguments}, tool output: {tool_call.tool_results}"
+        )
+
+    return "\n----\n".join(results)
--- src/tools/weather/weather_tool.py
+++ src/tools/weather/weather_tool.py
@@ -8,91 +8,79 @@ import logging
 from src.ai.tools.tool_registry import register_tool
 
 
-class WeatherTool:
-    @register_tool(
-        display_name="Weather",
-        help_text="Queries the weather at a given location.",
-        requires_documents=False,
-        description="Queries the weather at a given location.",
-        additional_instructions="Location is a string representing the City, State, and Country (if outside the US) of the location to get the weather for, e.g. 'Phoenix, AZ'. Date is optional, and should be a string ('%Y-%m-%d') representing the date to get the weather for, e.g. '2023-4-15'.  If no date is provided, the weather for the current date will be returned.",
-    )
-    def get_weather(self, location: str, date: str = None) -> str:
-        """Get the weather for a location and date.
-        Location is required, and should be a string representing the City, State, and Country (if outside the US) of the location to get the weather for, e.g. "Phoenix, AZ".
-        Date is optional, and should be a string ("%Y-%m-%d") representing the date to get the weather for, e.g. "2023-4-15".  If no date is provided, the weather for the current date will be returned.
-        """
+@register_tool(
+    display_name="Weather",
+    help_text="Queries the weather at a given location.",
+    requires_documents=False,
+    description="Queries the weather at a given location.",
+    additional_instructions="Location is a string representing the City, State, and Country (if outside the US) of the location to get the weather for, e.g. 'Phoenix, AZ'. Date is optional, and should be a string ('%Y-%m-%d') representing the date to get the weather for, e.g. '2023-4-15'.  If no date is provided, the weather for the current date will be returned.",
+)
+def get_weather(location: str, date: str = None) -> str:
+    """Get the weather for a location and date.
+    Location is required, and should be a string representing the City, State, and Country (if outside the US) of the location to get the weather for, e.g. "Phoenix, AZ".
+    Date is optional, and should be a string ("%Y-%m-%d") representing the date to get the weather for, e.g. "2023-4-15".  If no date is provided, the weather for the current date will be returned.
+    """
 
-        logging.debug(f"Weather Query: {location}, {date}")
+    logging.debug(f"Weather Query: {location}, {date}")
+
+    try:
+        # Parse the date string from the query
+        date_format = "%Y-%m-%d"
+        try:
+            if date is not None:
+                parsed_date = datetime.strptime(date, date_format).date()
+            else:
+                parsed_date = datetime.now().date()
+        except:
+            parsed_date = None
 
         try:
-            # Parse the date string from the query
-            date_format = "%Y-%m-%d"
-            try:
-                if date is not None:
-                    parsed_date = datetime.strptime(date, date_format).date()
-                else:
-                    parsed_date = datetime.now().date()
-            except:
-                parsed_date = None
+            result = get_or_create_eventloop().run_until_complete(get(location))
+        except asyncio.TimeoutError:
+            return "Timeout: The operation took too long to complete."
 
-            try:
-                result = self.get_or_create_eventloop().run_until_complete(
-                    self.get(location)
-                )
-            except asyncio.TimeoutError:
-                return "Timeout: The operation took too long to complete."
-
-            if parsed_date is None or parsed_date == datetime.now().date():
-                logging.debug("Looking for the current weather")
-                return f"The temperature for {location} is {result.current.temperature} degrees. Feels like: {result.current.feels_like} degrees. Description: {result.current.description}. Humidity: {result.current.humidity}."
-            else:
-                logging.debug(
-                    "Looking for a forecast for the date: " + str(parsed_date)
-                )
-                # Look for the date in the forecast
-                for forecast in result.forecasts:
-                    logging.debug("Forecast date: " + str(forecast.date))
-                    if forecast.date == parsed_date:
-                        return (
-                            f"The hourly forecast for {location} on {forecast.date}:\n"
-                            + "\n".join(
-                                [
-                                    f"Time: {fc.time}, temp: {fc.temperature}, description: {fc.description}"
-                                    for fc in forecast.hourly
-                                ]
-                            )
+        if parsed_date is None or parsed_date == datetime.now().date():
+            logging.debug("Looking for the current weather")
+            return f"The temperature for {location} is {result.current.temperature} degrees. Feels like: {result.current.feels_like} degrees. Description: {result.current.description}. Humidity: {result.current.humidity}."
+        else:
+            logging.debug("Looking for a forecast for the date: " + str(parsed_date))
+            # Look for the date in the forecast
+            for forecast in result.forecasts:
+                logging.debug("Forecast date: " + str(forecast.date))
+                if forecast.date == parsed_date:
+                    return (
+                        f"The hourly forecast for {location} on {forecast.date}:\n"
+                        + "\n".join(
+                            [
+                                f"Time: {fc.time}, temp: {fc.temperature}, description: {fc.description}"
+                                for fc in forecast.hourly
+                            ]
                         )
+                    )
 
-                return f"Could not find a forecast for {parsed_date}."
-        except Exception as e:
-            logging.error(e)
-            return "An error occurred while processing your request."
+            return f"Could not find a forecast for {parsed_date}."
+    except Exception as e:
+        logging.error(e)
+        return "An error occurred while processing your request."
 
-    async def get(self, query):
-        weather_client = python_weather.Client(unit=python_weather.IMPERIAL)
 
-        # Get the current weather of the city
-        weather = await weather_client.get(query)
+async def get(query):
+    weather_client = python_weather.Client(unit=python_weather.IMPERIAL)
 
-        # Return the weather
-        return weather
+    # Get the current weather of the city
+    weather = await weather_client.get(query)
 
-    def get_or_create_eventloop(self) -> asyncio.AbstractEventLoop:
-        try:
+    # Return the weather
+    return weather
+
+
+def get_or_create_eventloop() -> asyncio.AbstractEventLoop:
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
             return asyncio.get_event_loop()
-        except RuntimeError as ex:
-            if "There is no current event loop in thread" in str(ex):
-                loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(loop)
-                return asyncio.get_event_loop()
-            else:
-                raise ex
-
-
-# Test code
-if __name__ == "__main__":
-    tool = WeatherTool()
-
-    result = tool.run('{"location": "San Diego", "date": "2023-07-29"}')
-
-    print(result)
+        else:
+            raise ex
