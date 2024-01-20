9d733a62ab8ac3183f38bb38ca129813b7074d6c
aronweiler <aronweiler@gmail.com>
2024-01-20 09:54:52 -0800

    Update document loaders import

--- src/tools/web/wikipedia_tool.py
+++ src/tools/web/wikipedia_tool.py
@@ -1,23 +1,25 @@
-from langchain_community.tools import WikipediaQueryRun
-from langchain.utilities import WikipediaAPIWrapper
+from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
+from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 
 
 class WikipediaTool:
-
-    def search_wikipedia(self, query: str, top_k_results:int, doc_content_chars_max:int) -> str:
+    def search_wikipedia(
+        self, query: str, top_k_results: int, doc_content_chars_max: int
+    ) -> str:
         """Searches wikipedia for the specified query
-        
+
         Args:
             query (str): The query to search for
             top_k_results (int): The maximum number of results to return
             doc_content_chars_max (int): The maximum number of characters to return for each result
-            
+
             Returns:
                 str: The results of the search"""
         try:
             wikipedia = WikipediaQueryRun(
                 api_wrapper=WikipediaAPIWrapper(
-                    top_k_results=top_k_results, doc_content_chars_max=doc_content_chars_max
+                    top_k_results=top_k_results,
+                    doc_content_chars_max=doc_content_chars_max,
                 )
             )
 
