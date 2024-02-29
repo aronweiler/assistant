4a52243c9cfe5354c6491c02e7434d8f7862dde3
aronweiler <aronweiler@gmail.com>
2024-02-29 10:26:25 -0800

    Update formatting instructions and example response

--- src/ai/prompts/openai_prompts/formatting_instructions.py
+++ src/ai/prompts/openai_prompts/formatting_instructions.py
@@ -1,17 +1,21 @@
 FORMATTING_INSTRUCTIONS = """# Formatting Instructions
 Please examine the following JSON schema, and ensure that your response adheres to this schema.  Additionally, please ensure that your response is a valid JSON object.
 
+## JSON Schema
 ```json
 {response_format}
 ```
 
 Please note that this is the JSON schema for the response- not an example response.  Your response should be a JSON object that adheres to this schema.
 {example_prompt}
-AI: Sure, here is your JSON object adhering to the above schema:
+# Response
+AI: Sure, here is your valid JSON object adhering to the above schema:
 """
 
 EXAMPLE_PROMPT = """
-Here is an example response:
+Here is an example response that should guide your output.
+
+## Example Response
 
 ```json
 {example}
