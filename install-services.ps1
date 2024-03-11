d2bb68c7a5196281a5433fab5cc3704c69355c43
aronweiler <aronweiler@gmail.com>
2024-03-11 13:06:55 -0700

    Update requirements and launch configuration

--- install-services.ps1
+++ install-services.ps1
@@ -6,4 +6,4 @@ if (-not (Test-Path -Path "assistant_services_venv")) {
 Invoke-Expression -Command .\assistant_services_venv\Scripts\activate
 python.exe -m pip install --upgrade pip
 
-pip install -r requirements-services.txt
+pip install -r src/services/requirements.txt
