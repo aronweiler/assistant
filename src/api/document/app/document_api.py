54c14dde8da7bc8e3a3e6453c47d7feb0351bfe8
aronweiler <aronweiler@gmail.com>
2024-03-13 21:55:15 -0700

    Refactor document ingestion endpoint

--- src/api/document/app/document_api.py
+++ src/api/document/app/document_api.py
@@ -3,7 +3,6 @@ from typing import List
 import logging
 
 
-from src.api.shared.models.document_ingestion_model import DocumentIngestionModel
 import src.workers.document_processing.app.document_ingestion_tasks as document_ingestion_tasks
 
 
@@ -12,26 +11,34 @@ router = APIRouter()
 
 @router.post("/ingest")
 async def ingest_documents(
-    data: DocumentIngestionModel,
+    active_collection_id: int,
+    overwrite_existing_files: bool,
+    split_documents: bool,
+    create_summary_and_chunk_questions: bool,
+    summarize_document: bool,
+    chunk_size: int,
+    chunk_overlap: int,
+    user_id: int,
     files: List[UploadFile] = File(...),
 ):
+    raise NotImplementedError("This endpoint is not implemented- sending this task directly to celery from the UI")
 
-    for file in data.files:
-        file_location = f"/app/shared/{file.filename}"
-        with open(file_location, "wb+") as file_object:
-            file_object.write(file.file.read())
+    # for file in files:
+    #     file_location = f"/app/shared/{file.filename}"
+    #     with open(file_location, "wb+") as file_object:
+    #         file_object.write(file.file.read())
 
-        # Hand off to Celery for processing each file separately
-        document_ingestion_tasks.process_document_task.delay(
-            active_collection_id=data.active_collection_id,
-            overwrite_existing_files=data.overwrite_existing_files,
-            split_documents=data.split_documents,
-            create_summary_and_chunk_questions=data.create_summary_and_chunk_questions,
-            summarize_document=data.summarize_document,
-            chunk_size=data.chunk_size,
-            chunk_overlap=data.chunk_overlap,
-            user_id=data.user_id,
-            file_path=file_location,
-        )
+    #     # Hand off to Celery for processing each file separately
+    #     document_ingestion_tasks.process_document_task.delay(
+    #         active_collection_id=active_collection_id,
+    #         overwrite_existing_files=overwrite_existing_files,
+    #         split_documents=split_documents,
+    #         create_summary_and_chunk_questions=create_summary_and_chunk_questions,
+    #         summarize_document=summarize_document,
+    #         chunk_size=chunk_size,
+    #         chunk_overlap=chunk_overlap,
+    #         user_id=user_id,
+    #         file_path=file_location,
+    #     )
 
-    return {"message": "Documents are being processed"}
+    # return {"message": "Documents are being processed"}
