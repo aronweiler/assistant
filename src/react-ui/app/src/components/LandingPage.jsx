98b613d9db2a5aa5fedc7a5c6a903852ddf41bb9
aronweiler <aronweiler@gmail.com>
2024-03-15 00:27:26 -0700

    Add logout functionality to LandingPage component

--- src/react-ui/app/src/components/LandingPage.jsx
+++ src/react-ui/app/src/components/LandingPage.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import { useAuth } from '../context/AuthContext';
 
 function LandingPage() {
+  const { logout } = useAuth();
+
   return (
     <div>
       <h1>Hello World</h1>
       <Link to="/jarvis">Go to Jarvis Page</Link>
+      <br />
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
