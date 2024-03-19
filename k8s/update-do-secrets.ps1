3d90679056da38c85fad78df607cf81d0e769dc4
aronweiler <aronweiler@gmail.com>
2024-03-19 15:22:00 -0700

    Update Kubernetes configuration files

--- k8s/update-do-secrets.ps1
+++ k8s/update-do-secrets.ps1
@@ -1,18 +1,11 @@
 $envFilePath = './env.secrets'
 $secretName = 'jarvis-secrets'
-$nameSpace = 'jarvis'
-
-# Check if the namespace exists
-$namespaceExists = kubectl get namespace $nameSpace -o json --ignore-not-found
-if (-not $namespaceExists) {
-    kubectl create namespace $nameSpace
-}
 
 # Read each line from the .env file
 $lines = Get-Content $envFilePath
 
 # Create the secret (delete if exists)
-kubectl delete secret $secretName -n $nameSpace --ignore-not-found
+kubectl delete secret $secretName --ignore-not-found
 $arguments = @()
 foreach ($line in $lines) {
     if (-not [string]::IsNullOrWhiteSpace($line)) {
@@ -22,4 +15,4 @@ foreach ($line in $lines) {
         $arguments += "--from-literal=$key=$value"
     }
 }
-kubectl create secret generic $secretName -n $nameSpace @arguments
+kubectl create secret generic $secretName @arguments
